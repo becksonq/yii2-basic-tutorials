5757d78fda2405bce5695e497ff6735c741986e4
R.beckson <becksonq@gmail.com>
2017-09-27 19:17:31 +0300

    set_migrations

--- views/rooms/indexFiltered.php
+++ views/rooms/indexFiltered.php
@@ -5,7 +5,9 @@
  * Date: 25.09.2017
  * Time: 21:47
  */
+error_reporting(E_ALL);
 
+ini_set('display_errors', 'on');
 use yii\helpers\Url;
 
 $operators = ['=', '<=', '>='];
@@ -24,20 +26,24 @@ $sf = $searchFilter;
 		<div class="col-md-3">
 			<label>Floor</label>
 			<br/>
+
 			<select name="SearchFilter[floor][operator]">
 				<?php foreach ($operators as $op) { ?>
 					<?php $selected = ($operator == $op) ? 'selected' : ''; ?>
-					<option value="<?= $op ?>"
-							<?= $selected ?>><?= $op ?></option>
-				<?php } ?>=
+					<option value="<?= $op ?>" <?= $selected ?>><?= $op ?></option>
+				<?php } ?> =
 			</select>
+
 			<input type="text" name="SearchFilter[floor][value]" value="<?= $value ?>"/>
+
 		</div>
 		<?php $operator = $sf[ 'room_number' ][ 'operator' ]; ?>
 		<?php $value = $sf[ 'room_number' ][ 'value' ]; ?>
+
 		<div class="col-md-3">
 			<label>Room Number</label>
 			<br/>
+
 			<select name="SearchFilter[room_number][operator]">
 				<?php foreach ($operators as $op) { ?>
 					<?php $selected = ($operator == $op) ? 'selected' : ''; ?>
@@ -45,13 +51,16 @@ $sf = $searchFilter;
 							<?= $selected ?>><?= $op ?></option>
 				<?php } ?>
 			</select>
+
 			<input type="text" name="SearchFilter[room_number][value]" value="<?= $value ?>"/>
 		</div>
 		<?php $operator = $sf[ 'price_per_day' ][ 'operator' ]; ?>
 		<?php $value = $sf[ 'price_per_day' ][ 'value' ]; ?>
+
 		<div class="col-md-3">
 			<label>Price per day</label>
 			<br/>
+
 			<select name="SearchFilter[price_per_day][operator]">
 				<?php foreach ($operators as $op) { ?>
 					<?php $selected = ($operator ==
@@ -60,16 +69,17 @@ $sf = $searchFilter;
 							<?= $selected ?>><?= $op ?></option>
 				<?php } ?>
 			</select>
-			<input type="text"
-						 name="SearchFilter[price_per_day][value]"
-						 value="<?= $value ?>"/>
+
+			<input type="text" name="SearchFilter[price_per_day][value]" value="<?= $value ?>"/>
+
 		</div>
 	</div>
 	<br/>
+
 	<div class="row">
 		<div class="col-md-3">
-			<input type="submit" value="filter" class="btn btnprimary"/>
-			<input type="reset" value="reset" class="btn btnprimary"/>
+			<input type="submit" value="filter" class="btn btn-primary"/>
+			<input type="reset" value="reset" class="btn btn-primary"/>
 		</div>
 	</div>
 </form>
